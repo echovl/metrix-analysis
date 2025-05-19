32b91359b1bfb9930250642977f5f3b4a17b79e5
Alonso Villegas <alonsovillegas816@gmail.com>
2025-05-19 01:12:59 -0500

    add anova results

--- app/pucp_metrics.py
+++ app/pucp_metrics.py
@@ -1,6 +1,8 @@
 import os
 from pprint import pprint
 
+from sklearn.feature_selection import SelectKBest, VarianceThreshold, f_classif
+
 os.environ["TF_USE_LEGACY_KERAS"] = "1"
 
 from collections import OrderedDict
@@ -213,6 +215,56 @@ def train_model(
     print(training_output.head())
 
 
+def compute_anova_for_all_metrics():
+    train_dataset = load_dataset(
+        "symanto/autextification2023", "detection_es", split="train"
+    )
+
+    train_multiazter_df = pd.read_csv(
+        "./datasets/autextification_train_multiazter_indicators.csv", index_col="index"
+    )
+    train_cohmetrix_df = pd.read_csv(
+        "./datasets/autextification_train_cohmetrix_indicators.csv", index_col="index"
+    )
+    train_pucpmetrix_df = pd.read_csv(
+        "./datasets/autextification_train_pucp_indicators.csv", index_col="index"
+    )
+
+    train_labels = [data["label"] for data in train_dataset]
+
+    repositories = {
+        "multiazter": train_multiazter_df,
+        "coh_metrix": train_cohmetrix_df,
+        "pucp_metrix": train_pucpmetrix_df,
+    }
+
+    for repo_name, df_feats in repositories.items():
+        vt = VarianceThreshold(threshold=0.0)
+        df_feats = pd.DataFrame(
+            vt.fit_transform(df_feats), columns=df_feats.columns[vt.get_support()]
+        )
+
+        feature_names = df_feats.columns
+        selector = SelectKBest(score_func=f_classif, k="all")
+
+        selector.fit(df_feats.to_numpy(), train_labels)
+
+        f_scores = selector.scores_
+        p_values = selector.pvalues_
+
+        feature_ranking = sorted(
+            zip(feature_names, f_scores, p_values), key=lambda tpl: tpl[1], reverse=True
+        )
+
+        print(f"ANOVA for {repo_name} features:")
+        for name, f, p in feature_ranking:
+            print(f"\t{name:20s}  F-score = {f:.2f}  p-value = {p:.3g}")
+
+        pd.DataFrame(feature_ranking, columns=["feature", "f-score", "p-value"]).to_csv(
+            f"./results/anova_{repo_name}_features.csv", index=False
+        )
+
+
 def train_ml_models():
     train_dataset = load_dataset(
         "symanto/autextification2023", "detection_es", split="train"
@@ -327,6 +379,7 @@ def train_multiazter_model():
 if __name__ == "__main__":
     # train_berta_multiazter_model()
     # compute_and_save_pucp_metrics()
-    train_ml_models()
+    # train_ml_models()
+    compute_anova_for_all_metrics()
     # train_berta_pucp_model()
     # train_multiazter_model()

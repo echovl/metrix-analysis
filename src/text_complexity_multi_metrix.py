81d622c08523002d356da3bc4e1214a046ae1130
Alonso Villegas <alonsovillegas816@gmail.com>
2025-07-22 11:46:29 -0500

    add multi model

--- src/text_complexity_multi_metrix.py
+++ src/text_complexity_multi_metrix.py
@@ -5,6 +5,8 @@ os.environ["TF_USE_LEGACY_KERAS"] = "1"
 
 import numpy as np
 import pandas as pd
+import tensorflow as tf
+import tensorflow.keras as keras
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score, precision_score, recall_score
@@ -13,6 +15,9 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import RobustScaler
 from sklearn.svm import LinearSVC
 from sklearn.utils import shuffle
+from tensorflow.keras import layers
+from tensorflow.keras.optimizers import Adam
+from transformers import AutoTokenizer, TFRobertaModel
 from xgboost import XGBClassifier
 
 from dataloader import (
@@ -23,6 +28,148 @@ from dataloader import (
 
 LABEL_SIMPLE = 1
 LABEL_COMPLEX = 0
+
+
+def create_roberta_model(learning_rate: float):
+    input_ids = layers.Input(shape=(128,), dtype=tf.int32, name="input_ids")
+    attention_mask = layers.Input(shape=(128,), dtype=tf.int32, name="attention_mask")
+
+    roberta_model = TFRobertaModel.from_pretrained(
+        "PlanTL-GOB-ES/roberta-base-bne",
+        from_pt=True,
+        output_hidden_states=True,
+    )
+
+    outputs = roberta_model(input_ids, attention_mask=attention_mask)
+    cls_output = outputs.hidden_states[-1][:, 0, :]
+    output = layers.Dense(3, activation="softmax")(cls_output)
+
+    model = keras.Model(inputs=[input_ids, attention_mask], outputs=output)
+
+    model.compile(
+        optimizer=Adam(learning_rate=learning_rate),
+        metrics=["accuracy"],
+        loss="sparse_categorical_crossentropy",
+    )
+
+    return model, roberta_model
+
+
+def train_roberta_model():
+    train_texts, train_labels, val_texts, val_labels, test_texts, test_labels = (
+        load_text_complexity_multi_level_dataset()
+    )
+
+    lr = 3e-5
+    epochs = 3
+    batch_size = 32
+    tokenizer = AutoTokenizer.from_pretrained("PlanTL-GOB-ES/roberta-base-bne")
+
+    def tokenize(texts: list[str]):
+        return tokenizer(
+            texts, return_tensors="tf", padding=True, max_length=128, truncation=True
+        )
+
+    x_train_tokenized = tokenize(list(train_texts))
+    x_val_tokenized = tokenize(list(val_texts))
+    x_test_tokenized = tokenize(list(test_texts))
+    model, roberta_model = create_roberta_model(learning_rate=lr)
+    history = model.fit(
+        {
+            "input_ids": x_train_tokenized["input_ids"],
+            "attention_mask": x_train_tokenized["attention_mask"],
+        },
+        train_labels,
+        validation_data=(
+            {
+                "input_ids": x_val_tokenized["input_ids"],
+                "attention_mask": x_val_tokenized["attention_mask"],
+            },
+            val_labels,
+        ),
+        epochs=epochs,
+        batch_size=batch_size,
+        verbose=1,
+    )
+
+    train_pred = model.predict(
+        {
+            "input_ids": x_train_tokenized["input_ids"],
+            "attention_mask": x_train_tokenized["attention_mask"],
+        }
+    )
+    train_output = np.argmax(train_pred, axis=1).astype(int)
+
+    val_pred = model.predict(
+        {
+            "input_ids": x_val_tokenized["input_ids"],
+            "attention_mask": x_val_tokenized["attention_mask"],
+        }
+    )
+    # val_output = (val_pred > 0.5).astype(int)
+    val_output = np.argmax(val_pred, axis=1).astype(int)
+
+    test_pred = model.predict(
+        {
+            "input_ids": x_test_tokenized["input_ids"],
+            "attention_mask": x_test_tokenized["attention_mask"],
+        }
+    )
+    # test_output = (test_pred > 0.5).astype(int)
+    test_output = np.argmax(test_pred, axis=1).astype(int)
+
+    def get_scores(y_true, y_pred):
+        f1_macro = f1_score(y_true, y_pred, average="macro")
+        basic_f1, intermediate_f1, advanced_f1 = f1_score(y_true, y_pred, average=None)
+        basic_rec, intermediate_rec, advanced_rec = recall_score(
+            y_true, y_pred, average=None
+        )
+        basic_prec, intermediate_prec, advanced_prec = precision_score(
+            y_true, y_pred, average=None
+        )
+        return {
+            "f1_macro": f1_macro,
+            "basic_f1": basic_f1,
+            "intermediate_f1": intermediate_f1,
+            "advanced_f1": advanced_f1,
+            "basic_precision": basic_prec,
+            "intermediate_precision": intermediate_prec,
+            "advanced_precision": advanced_prec,
+            "basic_recall": basic_rec,
+            "intermediate_recall": intermediate_rec,
+            "advanced_recall": advanced_rec,
+        }
+
+    domains = {
+        "train": [train_labels, train_output],
+        "test": [test_labels, test_output],
+        "val": [val_labels, val_output],
+    }
+
+    model_results = {
+        "model": ["roberta"],
+    }
+
+    for domain, data in domains.items():
+        y_true, y_pred = data
+        scores = get_scores(y_true, y_pred)
+
+        print(f"Roberta {domain} scores: \n")
+        pprint(scores)
+        print("\n")
+
+        for key, value in scores.items():
+            result_key = f"{domain}_{key}"
+
+            if result_key not in model_results:
+                model_results[result_key] = []
+            model_results[result_key].append(value)
+
+    training_output = pd.DataFrame(model_results)
+    training_output.to_csv("./results/text_complexity_roberta_ml.csv")
+
+    print("Training results for Roberta:")
+    print(training_output.head())
 
 
 def train_model(
@@ -222,4 +369,5 @@ def train_ml_models():
 
 
 if __name__ == "__main__":
-    train_ml_models()
+    # train_ml_models()
+    train_roberta_model()
